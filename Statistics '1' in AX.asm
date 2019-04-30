1dd44063ef706ef254d9ecf58a9c098af1763785
Kirisame Marisa <kirisame.marisa.age.16@gmail.com>
2019-04-30 09:04:58 +0800

    2019-4-30

---
+++ Statistics '1' in AX.asm
@@ -0,0 +1,31 @@
+DATAS SEGMENT
+    ;此处输入数据段代码  
+DATAS ENDS
+
+STACKS SEGMENT
+    ;此处输入堆栈段代码
+STACKS ENDS
+
+CODES SEGMENT
+    ASSUME CS:CODES,DS:DATAS,SS:STACKS
+START:
+    MOV AX,DATAS
+    MOV DS,AX
+    
+    MOV AX,5
+    MOV BX,0
+    MOV CX,16
+    
+LOOP1:
+
+	ROR AX,1
+	JNC	NEXT1
+	INC BX
+
+NEXT1:
+	LOOP LOOP1
+    ;此处输入代码段代码
+    MOV AH,4CH
+    INT 21H
+CODES ENDS
+    END START
